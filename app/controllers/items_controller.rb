dbaaf4612c3cc438a0f987f1561e361b92d3e58b
Colter Ulrich <cju5025@gmail.com>
2021-01-06 13:05:24 -0700

    Adds create route to items controller.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,9 @@ class ItemsController < ApplicationController
         @items = Item.all 
         render json: @items
     end
+
+    def create
+        @new_item = Item.create name: params[:name], price: params[:price]
+        render json: @new_item
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :cart_items
-  resources :items
+  resources :cart_items, only: [:index, :create]
+  resources :items, only: [:index, :create]
   resources :users, only: [:index, :create]
   post "login", to: "authentication#login"
 end
